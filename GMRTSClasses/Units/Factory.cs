6bbc6e0941cbc653ac13cccea07e93f8599db0f6
Peter H <wappospypuppy@gmail.com>
2020-08-30 19:45:07 -0700

    Added Building and Factory classes.

---
+++ GMRTSClasses/Units/Factory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMRTSClasses.Units
+{
+    public class Factory : Building
+    {
+        public Factory() : base()
+        {
+
+        }
+
+        public Factory(Guid id) : base(id)
+        {
+
+        }
+    }
+}
