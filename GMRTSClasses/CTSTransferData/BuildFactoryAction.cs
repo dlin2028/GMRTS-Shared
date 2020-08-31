ccbd43f4e9e007313ae2af85d3bf3d299bcc58b0
Peter H <wappospypuppy@gmail.com>
2020-08-30 19:40:37 -0700

    Implemented more CTS actions.

---
+++ GMRTSClasses/CTSTransferData/BuildFactoryAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMRTSClasses.CTSTransferData
+{
+    public class BuildFactoryAction : ClientAction
+    {
+        public Vector2 Position { get; set; }
+    }
+}
---
+++ GMRTSClasses/CTSTransferData/PatrolAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMRTSClasses.CTSTransferData
+{
+    public class PatrolAction : ClientAction
+    {
+        public List<Vector2> Positions { get; set; }
+    }
+}
