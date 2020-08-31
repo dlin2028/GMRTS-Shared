d0a8d673ba0f8cfeb30773b227f1ba3c77ca04db
Peter H <wappospypuppy@gmail.com>
2020-08-30 19:15:11 -0700

    Made bare-bones classes for client-to-server communication.

---
+++ GMRTSClasses/CTSTransferData/ClientAction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMRTSClasses.CTSTransferData
+{
+    public abstract class ClientAction
+    {
+        public List<Guid> UnitIDs { get; set; }
+    }
+}
