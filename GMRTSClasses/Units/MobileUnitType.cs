5fe25b84b85abbc2d64e613a373072edf1ce7708
David Lin <dlin2028@gmail.com>
2021-05-23 19:37:04 -0700

    add UnitType

--- GMRTSClasses/Units/MobileUnitType.cs
+++ GMRTSClasses/Units/MobileUnitType.cs
@@ -4,9 +4,23 @@ using System.Text;
 
 namespace GMRTSClasses.Units
 {
+    public enum UnitType
+    {
+        Tank = 0,
+        Builder,
+        Factory,
+        Market,
+        Mine
+    }
     public enum MobileUnitType
     {
         Tank,
         Builder
+    }
+    public enum BuildingUnitType
+    {
+        Factory = 2,
+        Market,
+        Mine
     }
 }
